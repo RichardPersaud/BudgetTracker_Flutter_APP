e73f24b0fa9aec53c33f80923622b421370fef26
Richard Persaud <47072740+RichardPersaud@users.noreply.github.com>
2021-06-14 10:36:27 -0400

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,123 @@
+import 'package:budgettraker/budget_repo.dart';
+import 'package:budgettraker/failure_model.dart';
+import 'package:budgettraker/item_model.dart';
+import 'package:budgettraker/spending_chart.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:intl/intl.dart';
+
+void main() async {
+  await dotenv.load(fileName: '.env');
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Notion Budget Tracker',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
+      home: BudgetScreen(),
+    );
+  }
+}
+
+// ignore: use_key_in_widget_constructors
+class BudgetScreen extends StatefulWidget {
+  @override
+  _BudgetScreenState createState() => _BudgetScreenState();
+}
+
+class _BudgetScreenState extends State<BudgetScreen> {
+  late Future<List<Item>> _futureItems;
+
+  @override
+  void initState() {
+    super.initState();
+    _futureItems = BudgetRepository().getItems();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Budget Tracker'),
+        leading: Icon(Icons.account_circle_rounded),
+      ),
+      body: RefreshIndicator(
+        onRefresh: () async {
+          _futureItems = BudgetRepository().getItems();
+          setState(() {});
+        },
+        child: FutureBuilder<List<Item>>(
+          future: _futureItems,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              // Show pie chart and list view of items.
+              final items = snapshot.data!;
+              return ListView.builder(
+                itemCount: items.length + 1,
+                itemBuilder: (BuildContext context, int index) {
+                  if (index == 0) return SpendingChart(items: items);
+
+                  final item = items[index - 1];
+                  return Container(
+                    margin: const EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0),
+                      border: Border.all(
+                        width: 2.0,
+                        color: getCategoryColor(item.category),
+                      ),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.black26,
+                          offset: Offset(0, 2),
+                          blurRadius: 6.0,
+                        ),
+                      ],
+                    ),
+                    child: ListTile(
+                      title: Text(item.name),
+                      subtitle: Text(
+                        '${item.category} • ${DateFormat.yMd().format(item.date)}',
+                      ),
+                      trailing: Text(
+                        '-\$${item.price.toStringAsFixed(2)}',
+                      ),
+                    ),
+                  );
+                },
+              );
+            } else if (snapshot.hasError) {
+              // Show failure error message.
+              final failure = snapshot.error as Failure;
+              return Center(child: Text(failure.message));
+            }
+            // Show a loading spinner.
+            return const Center(child: CircularProgressIndicator());
+          },
+        ),
+      ),
+    );
+  }
+}
+
+Color getCategoryColor(String category) {
+  switch (category) {
+    case 'Entertainment':
+      return Colors.red[400]!;
+    case 'Food':
+      return Colors.green[400]!;
+    case 'Personal':
+      return Colors.blue[400]!;
+    case 'Transportation':
+      return Colors.purple[400]!;
+    default:
+      return Colors.orange[400]!;
+  }
+}
